98c92bf3ad4b8074e504568863692733bad42d6d
Diego Laballos <diego@diegolaballos.com>
2023-09-20 16:59:49 +0200

    Add iristick dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(files("../libs/iristick-sdk-trial-2.4.aar"))
 }
